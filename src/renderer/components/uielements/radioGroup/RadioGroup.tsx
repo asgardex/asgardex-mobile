28e413f9827f9aa3e8ebc1e14d3af6c887252167
cinnamoroll6130 <nextmission4260@gmail.com>
2025-03-06 15:51:05 +0900

    chore: update radio group component

--- src/renderer/components/uielements/radioGroup/RadioGroup.tsx
+++ src/renderer/components/uielements/radioGroup/RadioGroup.tsx
@@ -16,10 +16,10 @@ export const RadioGroup = ({
           <div
             key={option.value}
             className={clsx(
-              'cursor-pointer rounded-lg p-2',
+              'flex min-w-[64px] cursor-pointer items-center justify-center rounded-lg p-2',
               activeIndex === index
-                ? 'bg-turquoise dark:bg-turquoise-dark'
-                : 'bg-bg0 hover:bg-turquoise/20 dark:bg-bg0d hover:dark:bg-turquoise-dark/20'
+                ? 'bg-turquoise text-white dark:bg-turquoise-dark'
+                : 'bg-bg0 text-text2 hover:bg-turquoise/20 hover:text-turquoise dark:bg-bg0d dark:text-text2d hover:dark:bg-turquoise-dark/20 hover:dark:text-turquoise-dark'
             )}
             onClick={() => onChange(index)}>
             {option.label}
