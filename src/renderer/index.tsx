418130627489fe7bae2a0fefea780b8841366ca9
hippocampus-web3 <hippocampus.web3@proton.me>
2025-05-07 16:52:23 +0200

    Remive service worker

--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -4,7 +4,6 @@ import './index.css'
 import { createRoot } from 'react-dom/client'
 
 import { App } from './App'
-import * as serviceWorker from './serviceWorker'
 
 // // Registers custom headers (9R endpoints only)
 // register9Rheader('')
@@ -15,8 +14,3 @@ const container = document.getElementById('root')
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 const root = createRoot(container!) // createRoot(container!) if you use TypeScript
 root.render(<App />)
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister()
