f51406f1e1613cb01ebc3d133cc4f721ee6e7672
cinnamoroll6130 <nextmission4260@gmail.com>
2025-05-19 21:49:30 -0400

    chore: add locale

--- src/renderer/i18n/ko/tcy.ts
+++ src/renderer/i18n/ko/tcy.ts
@@ -1,18 +1,18 @@
 import { TcyMessages } from '../types'
 
 const tcy: TcyMessages = {
-  'tcy.claim': 'Claim',
-  'tcy.stake': 'Stake',
-  'tcy.unstake': 'Unstake',
-  'tcy.status': 'TCY Status',
-  'tcy.stakedAmount': 'Staked Amount',
-  'tcy.stakedAmountTooltip': 'Staked Amount includes your Keystore and Ledger Balances',
-  'tcy.walletBalance': 'Wallet Balance',
-  'tcy.walletBalanceTooltip': 'Wallet balance includes your Keystore and Ledger Balances',
-  'tcy.claimableAmount': 'Claimable Amount',
-  'tcy.claimNotice': 'You have these TCY tokens available to claim in your wallet.',
-  'tcy.stakeNotice': 'You can stake them to earn RUNE.',
-  'tcy.unstakeNotice': 'Unstake to move your TCY to your wallet.'
-}
+  'tcy.claim': '청구',
+  'tcy.stake': '스테이킹',
+  'tcy.unstake': '언스테이킹',
+  'tcy.status': 'TCY 상태',
+  'tcy.stakedAmount': '스테이킹된 수량',
+  'tcy.stakedAmountTooltip': '스테이킹된 수량에는 키스토어 및 레저 잔액이 포함됩니다',
+  'tcy.walletBalance': '지갑 잔액',
+  'tcy.walletBalanceTooltip': '지갑 잔액에는 키스토어 및 레저 잔액이 포함됩니다',
+  'tcy.claimableAmount': '청구 가능한 수량',
+  'tcy.claimNotice': '청구 가능한 TCY 토큰이 지갑에 있습니다.',
+  'tcy.stakeNotice': '스테이킹하여 RUNE을 획득할 수 있습니다.',
+  'tcy.unstakeNotice': 'TCY를 지갑으로 옮기려면 언스테이킹하세요.'
+};
 
 export default tcy
