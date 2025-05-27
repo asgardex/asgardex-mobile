bece852bfff611e0681e85ef2202d80978c6fc0b
cinnamoroll6130 <nextmission4260@gmail.com>
2025-05-27 14:41:55 +0900

    feat: add protocol switch for swap quote (#737)
    * feat: add redux actions for protocols
    
    * feat: add liquidity provider switch modal
    
    * feat: add storage helper
    
    * chore: update various styles

--- src/renderer/components/uielements/assets/assetMenu/AssetMenu.tsx
+++ src/renderer/components/uielements/assets/assetMenu/AssetMenu.tsx
@@ -234,7 +234,7 @@ export const AssetMenu: React.FC<Props> = (props): JSX.Element => {
             enter="ease"
             enterFrom="opacity-0 scale-95"
             enterTo="opacity-100 scale-100"
-            leave="ease-"
+            leave="ease"
             leaveFrom="opacity-100 scale-100"
             leaveTo="opacity-0 scale-95">
             <Dialog.Panel
