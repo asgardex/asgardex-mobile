3d89becfa6a47d97a3ed8baa537374fa7e5a5edf
cinnamoroll6130 <nextmission4260@gmail.com>
2025-05-30 00:44:27 +0900

    feaT: add import phrase view

---
+++ src/renderer/views/wallet/ImportPhraseView/ImportPhraseView.tsx
@@ -0,0 +1,73 @@
+import React, { useEffect, useMemo } from 'react'
+
+import * as RD from '@devexperts/remote-data-ts'
+import { useObservableState } from 'observable-hooks'
+import { useIntl } from 'react-intl'
+import { useNavigate } from 'react-router-dom'
+
+import { HeaderTheme } from '../../../components/header/theme'
+import { BackLinkButton } from '../../../components/uielements/button'
+import { Headline } from '../../../components/uielements/headline'
+import { ImportPhrase } from '../../../components/wallet/phrase'
+import { useWalletContext } from '../../../contexts/WalletContext'
+import { getWalletNamesFromKeystoreWallets } from '../../../helpers/walletHelper'
+import { useKeystoreClientStates } from '../../../hooks/useKeystoreClientStates'
+import { useKeystoreWallets } from '../../../hooks/useKeystoreWallets'
+import * as walletRoutes from '../../../routes/wallet'
+import { generateKeystoreId } from '../../../services/wallet/util'
+
+export const ImportPhraseView: React.FC = (): JSX.Element => {
+  const intl = useIntl()
+  const navigate = useNavigate()
+
+  const {
+    keystoreService: { addKeystoreWallet, importingKeystoreState$, resetImportingKeystoreState }
+  } = useWalletContext()
+
+  const importingKeystoreState = useObservableState(importingKeystoreState$, RD.initial)
+
+  const { clientStates } = useKeystoreClientStates()
+
+  const { walletsUI } = useKeystoreWallets()
+  const walletNames = useMemo(() => getWalletNamesFromKeystoreWallets(walletsUI), [walletsUI])
+
+  // Reset `ImportingKeystoreState` by entering the view
+  useEffect(() => {
+    resetImportingKeystoreState()
+  }, [resetImportingKeystoreState])
+
+  // Redirect after successful import
+  useEffect(() => {
+    if (RD.isSuccess(importingKeystoreState)) {
+      resetImportingKeystoreState()
+      // redirect to wallets assets view
+      navigate(walletRoutes.assets.path())
+    }
+  }, [navigate, importingKeystoreState, resetImportingKeystoreState])
+
+  const walletId = useMemo(() => generateKeystoreId(), [])
+
+  return (
+    <div className="relative flex flex-col items-center justify-center h-full w-full bg-bg1 dark:bg-bg1d">
+      <div className="absolute top-4 left-4 z-10">
+        <BackLinkButton />
+      </div>
+      <div className="absolute top-4 right-4 z-10">
+        <HeaderTheme isDesktopView />
+      </div>
+
+      <div className="flex flex-col p-4 w-full max-w-[380px]">
+        <div className="flex items-center mb-10">
+          <Headline>{intl.formatMessage({ id: 'wallet.imports.wallet' })}</Headline>
+        </div>
+
+        <ImportPhrase
+          walletId={walletId}
+          walletNames={walletNames}
+          clientStates={clientStates}
+          addKeystore={addKeystoreWallet}
+        />
+      </div>
+    </div>
+  )
+}
