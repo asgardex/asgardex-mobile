8952745e364ec4b51a72e121b32fc2e04063dd31
cinnamoroll6130 <nextmission4260@gmail.com>
2025-05-29 13:46:17 +0900

    chore: update no wallet view

--- src/renderer/views/wallet/CreateView/CreateView.tsx
+++ src/renderer/views/wallet/CreateView/CreateView.tsx
@@ -3,10 +3,10 @@ import { useMemo } from 'react'
 import { useIntl } from 'react-intl'
 import { useNavigate } from 'react-router-dom'
 
-import { PageTitle } from '../../../components/page'
-import { Tabs } from '../../../components/tabs'
+import { HeaderTheme } from '../../../components/header/theme'
+import { BackLinkButton } from '../../../components/uielements/button'
+import { Headline } from '../../../components/uielements/headline'
 import * as walletRoutes from '../../../routes/wallet'
-import * as Styled from './CreateView.styles'
 import { PhraseView } from './PhraseView'
 
 enum TabKey {
@@ -33,10 +33,22 @@ export const CreateView = () => {
     [navigate, intl]
   )
 
+  console.log('ITEMS - ', items)
+
   return (
-    <Styled.Container>
-      <PageTitle>{intl.formatMessage({ id: 'wallet.create.title' })}</PageTitle>
-      <Tabs tabs={items} defaultTabIndex={0} />
-    </Styled.Container>
+    <div className="relative flex flex-col items-center justify-center h-full w-full bg-bg1 dark:bg-bg1d">
+      <div className="absolute top-4 left-4 z-10">
+        <BackLinkButton />
+      </div>
+      <div className="absolute top-4 right-4 z-10">
+        <HeaderTheme isDesktopView />
+      </div>
+      <div className="flex flex-col p-4 w-full max-w-[380px]">
+        <div className="flex items-center mb-10">
+          <Headline>{intl.formatMessage({ id: 'wallet.create.title' })}</Headline>
+        </div>
+        <PhraseView />
+      </div>
+    </div>
   )
 }
