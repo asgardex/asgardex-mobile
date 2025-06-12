d458fb6dab512c7e67dbbc867a983e4393443b96
cinnamoroll6130 <nextmission4260@gmail.com>
2025-06-12 22:25:51 +0900

    feat: add Dropdown component

---
+++ src/renderer/components/uielements/dropdown/Dropdown.tsx
@@ -0,0 +1,29 @@
+import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
+import { AnchorProps } from '@headlessui/react/dist/internal/floating'
+import clsx from 'clsx'
+
+type Props = {
+  className?: string
+  trigger: React.ReactNode
+  options: React.ReactNode[]
+  anchor?: AnchorProps
+}
+
+export const Dropdown = ({ className, trigger, options, anchor = 'bottom start' }: Props) => {
+  return (
+    <Menu>
+      <MenuButton>{trigger}</MenuButton>
+      <MenuItems
+        transition
+        anchor={anchor}
+        className={clsx(
+          'z-10 origin-top-right rounded-xl bg-bg0 dark:bg-bg0d p-2 border border-solid border-gray0 dark:border-gray0d text-sm/6 text-white transition duration-100 ease-out [--anchor-gap:--spacing(1)] focus:outline-none data-closed:scale-95 data-closed:opacity-0',
+          className
+        )}>
+        {options.map((option, index) => (
+          <MenuItem key={index}>{option}</MenuItem>
+        ))}
+      </MenuItems>
+    </Menu>
+  )
+}
