04b159246b0b41083904571bcd5106f8da6928f0
cinnamoroll6130 <nextmission4260@gmail.com>
2025-10-15 01:15:30 +0900

    chore: update stepbar comp

--- src/renderer/components/uielements/stepBar/StepBar.tsx
+++ src/renderer/components/uielements/stepBar/StepBar.tsx
@@ -1,4 +1,4 @@
-import * as Styled from './StepBar.styles'
+import clsx from 'clsx'
 
 export type Props = {
   size?: number
@@ -7,10 +7,10 @@ export type Props = {
 
 export const StepBar = ({ size = 150, className }: Props): JSX.Element => {
   return (
-    <Styled.Container className={className}>
-      <Styled.Dot />
-      <Styled.Line size={size} />
-      <Styled.Dot />
-    </Styled.Container>
+    <div className={clsx('flex flex-col justify-center w-[9px]', className)}>
+      <div className="w-[9px] h-[9px] rounded-full bg-gray1 dark:bg-gray1d" />
+      <div className="w-[5px] border-r border-solid border-gray1 dark:border-gray1d" style={{ height: `${size}px` }} />
+      <div className="w-[9px] h-[9px] rounded-full bg-gray1 dark:bg-gray1d" />
+    </div>
   )
 }
